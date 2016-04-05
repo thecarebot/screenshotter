d18b46a251f0820a23584fea6972c30e7d73a418
Matt Hampel <matth@localdata.com>
2016-04-05 16:15:55 -0400

    Working express app

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ crop it to just that element.
 ## Run
 
 `node index.js`
+
+## Test request
+
+http://localhost:4432/api/image?id=storytext&url=http://www.npr.org/2016/03/28/470522433/welcome-to-rent-court-where-tenants-can-face-a-tenuous-fate
--- index.js
+++ index.js
@@ -1,89 +1,94 @@
+var express = require('express');
 var lwip = require('lwip');
 var phantom = require('phantom');
-// var page = require('webpage').create();
+var app = express();
 
-function process(error, image) {
-  if (error) {
-    console.log(error);
+var port = process.env.PORT || 4432;
+
+app.get('/api/image', function (req, res) {
+  var sitepage = null;
+  var phInstance = null;
+  var buffer = null;
+
+  var elId = req.query.id;
+  var url = req.query.url;
+
+  if (!elId || !url) {
+    res.send('Parameters url and id are required');
+    return;
   }
-}
 
-var elId = 'storytext';
-//id = 'main';
-var url = 'http://www.npr.org/2016/03/28/470522433/welcome-to-rent-court-where-tenants-can-face-a-tenuous-fate';
-// url = 'http://thecarebot.github.io';
-var sitepage = null;
-var phInstance = null;
-var buffer = null;
+  console.log("Handling request for", elId, url);
 
-// Setup
-phantom.create()
-.then((instance) => {
-  phInstance = instance;
-  return instance.createPage();
-})
+  // Setup
+  phantom.create()
+  .then((instance) => {
+    phInstance = instance;
+    return instance.createPage();
+  })
 
-.then((page) => {
-  sitepage = page;
-  return sitepage.property('viewportSize', {width: 1280, height: 1020});
-})
+  .then((page) => {
+    sitepage = page;
+    return sitepage.property('viewportSize', {width: 1280, height: 1020});
+  })
 
-// Set the viewport and open the page
-.then((page) => {
-  return sitepage.open(url);
-})
+  // Set the viewport and open the page
+  .then((page) => {
+    return sitepage.open(url);
+  })
 
-// Get the bounds of the page
-.then(function() {
-  // We run `evaluateJavaScript` instead of `evaluate` because
-  // there seems to be a bug passing local variables in to the page.
-  return sitepage.evaluateJavaScript(
-    'function() {return document.getElementById("' + elId + '").getBoundingClientRect(); }'
-  );
-})
+  // Get the bounds of the page
+  .then(function() {
+    // We run `evaluateJavaScript` instead of `evaluate` because
+    // there seems to be a bug passing local variables in to the page.
+    return sitepage.evaluateJavaScript(
+      'function() {return document.getElementById("' + elId + '").getBoundingClientRect(); }'
+    );
+  })
 
-// Using the bounds, clip the page
-.then(function(bounds) {
-    console.log(bounds.top, bounds.right, bounds.bottom, bounds.left);
+  // Using the bounds, clip the page
+  .then(function(bounds) {
+      console.log(bounds.top, bounds.right, bounds.bottom, bounds.left);
 
-    return sitepage.property('clipRect', {
-      top: bounds.top,
-      left: bounds.left,
-      width: bounds.width,
-      height: bounds.height
+      return sitepage.property('clipRect', {
+        top: bounds.top,
+        left: bounds.left,
+        width: bounds.width,
+        height: bounds.height
+      });
+  })
+
+  // Render the image
+  .then(function() {
+    return sitepage.renderBase64('PNG');
+  })
+
+  .then(function(buffer) {
+    b = new Buffer(buffer, 'base64');
+
+    res.writeHead({
+      'Content-Type': 'image/png',
+      'Content-Length': b.length
     });
-})
+    res.end(b);
 
-// Render the image
-.then(function() {
-  return sitepage.renderBase64('PNG');
-})
-
-.then(function(buffer) {
-  b = new Buffer(buffer, 'base64');
-
-  // Crop and save the images
-  lwip.open(b, 'png', function(error, image) {
-    if (error) {
-      console.log("Error opening image", error);
-      return;
-    }
-    image.writeFile('test.png', function(error) {
-      if (error) {
-        console.log("Error saving image", error);
-      }
-    });
     sitepage.close();
     phInstance.exit();
     return;
-  });
-})
+  })
 
-.catch((error) => {
-  console.log(error);
-  if (sitepage) {
-    sitepage.close();
-  }
-  phInstance.exit();
+  .catch((error) => {
+    console.log(error);
+    if (sitepage) {
+      sitepage.close();
+    }
+    phInstance.exit();
+
+    res.send(error);
+  });
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port', port);
 });
 

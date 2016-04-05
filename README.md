70512d49f9e10a712a2a93e5a2574ce4d9520b7a
Matt Hampel <matth@localdata.com>
2016-04-05 15:15:21 -0400

    Working implementation

---
+++ README.md
@@ -0,0 +1,12 @@
+# Simple screenshotter app
+
+Give it a URL and an element ID. It'll fetch the page, make a screenshot, and
+crop it to just that element.
+
+## Install
+
+`npm install`
+
+## Run
+
+`node index.js`
--- index.js
+++ index.js
@@ -8,70 +8,94 @@ function process(error, image) {
   }
 }
 
+var elId = 'storytext';
+//id = 'main';
+var url = 'http://www.npr.org/2016/03/28/470522433/welcome-to-rent-court-where-tenants-can-face-a-tenuous-fate';
+// url = 'http://thecarebot.github.io';
 var sitepage = null;
 var phInstance = null;
+var buffer = null;
+
+// Setup
 phantom.create()
-    .then((instance) => {
-        phInstance = instance;
-        return instance.createPage();
-    })
-    .then((page) => {
-        sitepage = page;
-        page.viewportSize = {
-          width: 1280,
-          height: 1020
-        };
-        return page.open('http://www.npr.org/2016/03/28/470522433/welcome-to-rent-court-where-tenants-can-face-a-tenuous-fate');
-        // return page.open('http://google.com');
-    })
-    .then((status) => {
-      sitepage.evaluate(function() {
-        // return document.documentElement.scrollTop;
-        document.body.style.width = 1280 + "px";
-        document.getElementById("storytext").style.border = "5px solid red";
-        var bounds = document.getElementById("storytext").getBoundingClientRect();
-        console.log(bounds.right, bounds.top);
-        // document.body.style.height = h + "px";
-        return;
-      })
-      .then(function() {
-        sitepage.evaluate(function() {
-            return document.getElementById("storytext").getBoundingClientRect();
-        })
-        .then(function(bounds) {
-        console.log(bounds.top, bounds.left, bounds.bottom, bounds.right);
+.then((instance) => {
+  phInstance = instance;
+  return instance.createPage();
+})
 
-        sitepage.renderBase64('PNG')
-        .then(function(buffer) {
-          console.log(typeof buffer);
-          var b = new Buffer(buffer, 'base64');
-          lwip.open(b, 'png', function(error, image) {
-            if (error) {
-              console.log(error);
-              return;
-            }
-            //image.crop(Math.round(bounds.left), Math.round(bounds.top), Math.round(bounds.right), Math.round(bounds.bottom), function(error, image) {
-            //  console.log("Did I crop?", error);
-              image.writeFile('test.png', function(error) {
-                console.log("Did I save?", error);
-              });
-            //});
-          });
+.then((page) => {
+  sitepage = page;
+  return sitepage.property('viewportSize', {width: 1280, height: 1020});
+})
 
-          sitepage.close();
-          phInstance.exit();
-          return;
-        })
-        .catch(function(error) {
-          console.log("Error getting bounds", error);
-          sitepage.close();
-          phInstance.exit();
-        });
-      });
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-      phInstance.exit();
+// Set the viewport and open the page
+.then((page) => {
+  return sitepage.open(url);
+})
+
+// Get the dimensions of the element we want.
+.then((status) => {
+  return sitepage.evaluate(function() {
+    // Force the page to the width we want.
+    // document.body.style.width = 1280 + "px";
+
+    // Highlight the article in red
+    console.log("Setting", elId)
+    document.getElementById(elId).style.border = "5px solid red";
+    return;
+  });
+})
+
+// Get the bounds of the page
+.then(function() {
+  // Get the bounds of the page
+  return sitepage.evaluateJavaScript(
+    'function() {return document.getElementById("' + elId + '").getBoundingClientRect(); }'
+  );
+})
+
+// Using the bounds, clip the page
+.then(function(bounds) {
+    console.log(bounds.top, bounds.right, bounds.bottom, bounds.left);
+
+    return sitepage.property('clipRect', {
+      top: bounds.top,
+      left: bounds.left,
+      width: bounds.width,
+      height: bounds.height
     });
+})
+
+// Render the image
+.then(function() {
+  return sitepage.renderBase64('PNG');
+})
+
+.then(function(buffer) {
+  b = new Buffer(buffer, 'base64');
+  console.log("Buffer created", b);
+
+  // Crop and save the images
+  lwip.open(b, 'png', function(error, image) {
+    if (error) {
+      console.log("Error opening image", error);
+      return;
+    }
+    image.writeFile('test.png', function(error) {
+      console.log("Did I save?", error);
+    });
+    console.log("Saved image");
+    sitepage.close();
+    phInstance.exit();
+    return;
+  });
+})
+
+.catch((error) => {
+  console.log(error);
+  if (sitepage) {
+    sitepage.close();
+  }
+  phInstance.exit();
+});
 
